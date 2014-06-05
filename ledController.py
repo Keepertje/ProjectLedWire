b241c08100b693a911284d2bf1d307278a45c167
CindyBerghuizen <CindyBerghuizen@users.noreply.github.com>
2014-06-05 10:52:00 +0200

    Transision function added
    Still need to test if it works :)

--- ledController.py
+++ ledController.py
@@ -3,12 +3,44 @@ from Ledstrip import Ledstrip
 class ledController():
 	
 	ledstrip = None
+	curR = None
+	curG = None
+	curB = None
+	
 	def __init__(self):
 		self.ledstrip = Ledstrip()
+		curR = 0
+		curG = 0
+		curB = 0
 
 	def changeColor(r,g,b):
 		color = ledstrip.Color(r,g,b)
-		allColor(color)
+		allColor(color,0.05)
+		
+	def transision(r,g,b):
+		diffR = r - curR
+		diffG = g - curG
+		diffB = b - curB
+		for i in range(abs(diffR)):
+			if(diffR > 0):
+				changeColor( curR + i,curG,curB)
+			else:
+				changeColor(curR - i, curG , curB)
+		curR = r
+		for i in range(abs(diffG)):
+			if(diffG > 0):
+				changeColor( curR ,curG + i,curB)
+			else:
+				changeColor(curR - i, curG - i, curB)
+		curG = g
+		for i in range(abs(diffB)):
+			if(diffB > 0):
+				changeColor(curR,curG,curB + i)
+			else:
+				changeColor(curR , curG , curB - i)
+		curR = b
+			
+				
 		
 	def allColor(c,delay):
 		for i in range(25):
